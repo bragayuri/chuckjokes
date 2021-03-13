9dee4be7fec9d1dd7267b870c413cd76db6e3c71
rochabragayuri <yuri.braga@apegro.com>
2021-03-13 11:48:13 +0000

    feat: with storybook

---
+++ src/stories/lib/types.ts
@@ -0,0 +1,15 @@
+export type Color =
+  | "transparent"
+  | "current"
+  | "black"
+  | "white"
+  | "gray"
+  | "red"
+  | "orange"
+  | "yellow"
+  | "green"
+  | "teal"
+  | "blue"
+  | "indigo"
+  | "purple"
+  | "pink";
